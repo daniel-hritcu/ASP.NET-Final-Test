2157efdb3794ba45538bf8abb286feb17d5e6a7f
Daniel Hritcu <daniel@hritcu.ca>
2020-05-29 17:02:00 -0400

    Add project files.

---
+++ PetStore.Domain/Models/Owner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PetStore.Domain.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        [NotMapped]
+        public string FullName 
+        {   
+            get => $"{FirstName} {LastName}";
+            set => FullName = value;
+        }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string EmailAddress { get; set; }
+        public string Occupation { get; set; }
+        public ICollection<Pet> Pets { get; set; }
+    }
+}
---
+++ PetStore.Domain/Models/Pet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetStore.Domain.Models
+{
+    public enum Breed {
+        Dog,
+        Cat,
+        Fish,
+        Hamster,
+        GuineaPig
+    }
+    public class Pet
+    {
+        public int Id { get; set; }
+        public DateTime DOB { get; set; }
+        public Breed Breed { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
